f37dc90ef58aa709263c4df9a8787668ebc59e1c
DevidurgaArunachalam <96167015+DevidurgaArunachalam@users.noreply.github.com>
2022-01-29 10:44:52 +0530

    Indentation Changed

--- src/com/employee/service/EmployeeServices.java
+++ src/com/employee/service/EmployeeServices.java
@@ -3,25 +3,26 @@ package com.employee.service;
 import java.util.Date;
 
 import com.employee.model.Employee;
+
 /**
- * Implements the methods from the  EmployeeServiceImpl class.
- */
+* Implements the methods from the  EmployeeServiceImpl class.
+*/
 public interface EmployeeServices {
     public void addEmployeeDetails(Employee employee);
 
-	public void viewEmployeeDetails();
+    public void viewEmployeeDetails();
 
-	public void updateEmployeeDetails(Employee employee);
+    public void updateEmployeeDetails(Employee employee);
 	
-	public void deleteEmployeeDetails(int employeeId);
+    public void deleteEmployeeDetails(int employeeId);
 	
-	public String checkEmployeeName(String employeeName);
+    public String checkEmployeeName(String employeeName);
 	
-	public String checkContactNumber(String contactNumber);
+    public String checkContactNumber(String contactNumber);
 	
-	public String checkEmailId(String emailId);
+    public String checkEmailId(String emailId);
 	
-	public String checkSalary(String salary);
+    public String checkSalary(String salary);
 	
-	public Date dateValidation(String dateOfBirth);
+    public Date dateValidation(String dateOfBirth);
 }
