4d4c8049b8216e6fd65bda59e21e3c61aebe5e2a
DevidurgaArunachalam <96167015+DevidurgaArunachalam@users.noreply.github.com>
2022-02-01 01:01:27 +0530

    Maven and Database updates

--- src/main/java/com/employee/service/EmployeeServices.java
+++ src/main/java/com/employee/service/EmployeeServices.java
@@ -4,27 +4,28 @@ import java.sql.Date;
 import java.sql.SQLException;
 
 import com.employee.model.Employee;
+
 /**
- * Implements the methods from the  EmployeeServiceImpl class.
- */
+* Implements the methods from the  EmployeeServiceImpl class.
+*/
 public interface EmployeeServices {
     public void addEmployee(Employee employee) throws SQLException;
 
-	public void viewEmployeeDetails() throws SQLException;
+    public void viewEmployeeDetails() throws SQLException;
 
-	public void updateEmployeeDetails(Employee employee) throws SQLException;
+    public void updateEmployeeDetails(Employee employee) throws SQLException;
 	
-	public void deleteEmployee(int employeeId) throws SQLException;
+    public void deleteEmployee(int employeeId) throws SQLException;
 	
-	public String checkEmployeeName(String employeeName);
+    public String checkEmployeeName(String employeeName);
 	
-	public String checkContactNumber(String contactNumber);
+    public String checkContactNumber(String contactNumber);
 	
-	public String checkEmailId(String emailId);
+    public String checkEmailId(String emailId);
 	
-	public String checkSalary(String salary);
+    public String checkSalary(String salary);
 	
-	public Date dateValidation(String dateOfBirth);
+    public Date dateValidation(String dateOfBirth);
 
     public void updateDetails(Employee employee) throws SQLException;
 }
