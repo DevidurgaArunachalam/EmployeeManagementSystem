d1a40ae11f0694496d150a9ddfa0929c97d8b5f2
DevidurgaArunachalam <96167015+DevidurgaArunachalam@users.noreply.github.com>
2022-02-02 23:18:35 +0530

    updated changes

--- src/main/java/com/employee/exception/DataNotFoundException.java
+++ src/main/java/com/employee/exception/DataNotFoundException.java
@@ -1,6 +1,12 @@
 package com.employee.exception;
-
+/**
+* Created custom exception
+*/
 public class DataNotFoundException  extends Exception {
+  
+  /**
+  * Method it show the custom exception
+  */
   public DataNotFoundException(String message) {
       super(message);
   }
