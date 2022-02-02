0e1afa414b64241ce8a662769995a9b896851590
DevidurgaArunachalam <96167015+DevidurgaArunachalam@users.noreply.github.com>
2022-02-02 23:43:06 +0530

    updated changes

--- src/main/java/com/employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/employee/service/EmployeeServiceImpl.java
@@ -16,15 +16,14 @@ import java.util.Map;
  * in insertion order maintenance and for better perrformance.
  */
 public class EmployeeServiceImpl implements EmployeeServices {
-	private final static Map<Integer, Employee> EMPLOYEEDETAILS = new LinkedHashMap<Integer, Employee>();
+    private final static Map<Integer, Employee> EMPLOYEEDETAILS = new LinkedHashMap<Integer, Employee>();
 	
-	/**
-	* Adds a new employee details. Here employee id is set as key andother details like 
-	* employee name, salary, contact number, emailId and dateofbirth are stored in employe object.
-	* @param employee Object
-	* @throws SQLException 
-	*/
-	public void addEmployee(Employee employee) {
+   /**
+    * Adds a new employee details. Here employee id is set as key andother details like 
+    * employee name, salary, contact number, emailId and dateofbirth are stored in employe object.
+    * @param employee Object
+    */
+    public void addEmployee(Employee employee) {
         int employeeId = employee.getEmployeeId();
         
         if (EMPLOYEEDETAILS.containsKey(employeeId)) {
@@ -35,20 +34,20 @@ public class EmployeeServiceImpl implements EmployeeServices {
     }
 
    /**
-	* Views employee details that are stored in the map and it gives both key and values.
-	*/
-	public void viewEmployeeDetails() {
+    * Views employee details that are stored in the map and it gives both key and values.
+    */
+    public void viewEmployeeDetails() {
         for (Integer data : EMPLOYEEDETAILS.keySet()) {
             System.out.println(EMPLOYEEDETAILS.get(data));
         }
     }
 
    /**
-	* Deletes the employee details by getting employeeId to check and delete.
-	* @param employeeId 
-    * @throws SQLException 
-	*/
-	public void deleteEmployee(int employeeId) throws DataNotFoundException{
+    * Deletes the employee details by getting employeeId to check and delete.
+    * @param employeeId 
+    * @throws DataNotFoundException 
+    */
+    public void deleteEmployee(int employeeId) throws DataNotFoundException{
 	    
         if(EMPLOYEEDETAILS.containsKey(employeeId)) {
             EMPLOYEEDETAILS.remove(employeeId); 
@@ -58,13 +57,13 @@ public class EmployeeServiceImpl implements EmployeeServices {
     }
 	
    /**
-	* Updates the employee details by getting the user choice to update as per
-	* their choice to update. Calls the methods in EmployeeManagement
-	* class to update as per user choice.
-	* @param employee
- * @throws DataNotFoundException 
-	*/
-	public void updateEmployeeDetails(Employee employee) throws DataNotFoundException {
+    * Updates the employee details by getting the user choice to update as per
+    * their choice to update. Calls the methods in EmployeeManagement
+    * class to update as per user choice.
+    * @param employee
+    * @throws DataNotFoundException 
+    */
+    public void updateEmployeeDetails(Employee employee) throws DataNotFoundException {
 	    
         if(EMPLOYEEDETAILS.containsKey(employee.getEmployeeId())) { 
             Employee employeeData = EMPLOYEEDETAILS.get(employee.getEmployeeId());
@@ -86,82 +85,82 @@ public class EmployeeServiceImpl implements EmployeeServices {
     }
 	
    /**
-	* Checks the employee name as accepting only alphabets and returns the 
-	* validated input to the Employee Details class.
-	* @return employeeName
-	*/
-	public String checkEmployeeName(String employeeName) {
+    * Checks the employee name as accepting only alphabets and returns the 
+    * validated input to the Employee Details class.
+    * @return employeeName
+    */
+    public String checkEmployeeName(String employeeName) {
 	    
-		if (!employeeName.matches("[A-Za-z]{1,}")) {
-			System.out.println("Invalid Input!!!...");
-			return EmployeeDetails.getEmployeeName();
-		} 
-		return employeeName;
-	}
+       if (!employeeName.matches("[A-Za-z]{1,}")) {
+	   System.out.println("Invalid Input!!!...");
+	   return EmployeeDetails.getEmployeeName();
+       } 
+	 return employeeName;
+    }
 
    /**
-	* Checks the contactNumber as accepting only numeric values as in the pattern and returns
-	* the validated input to the Employee details class.
-	* @return contactNumber
-	*/
-	public String checkContactNumber(String contactNumber) {
+    * Checks the contactNumber as accepting only numeric values as in the pattern and returns
+    * the validated input to the Employee details class.
+    * @return contactNumber
+    */
+    public String checkContactNumber(String contactNumber) {
 	    
-		if (!contactNumber.matches("[6-9]{1}[0-9]{9}")) {
-			System.out.println("Invalid Input!!!...");
-			return EmployeeDetails.getContactNumber();
-		} 
-		return contactNumber;
-	}
+	if (!contactNumber.matches("[6-9]{1}[0-9]{9}")) {
+	    System.out.println("Invalid Input!!!...");
+	    return EmployeeDetails.getContactNumber();
+	} 
+	   return contactNumber;
+    }
 
    /**
-	* Checks the emailId as accepting the below given pattern and return the 
-	* validated input to the Employee details class.
-	* @return emailId
-	*/
-	public String checkEmailId(String emailId) {
+    * Checks the emailId as accepting the below given pattern and return the 
+    * validated input to the Employee details class.
+    * @return emailId
+    */
+    public String checkEmailId(String emailId) {
 	    
-		if (!emailId.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
-			System.out.println("Invalid Input!!!...");
-			return EmployeeDetails.getEmailId();
-		} 
-		return emailId;
-	}
+	if (!emailId.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+	    System.out.println("Invalid Input!!!...");
+	    return EmployeeDetails.getEmailId();
+	} 
+           return emailId;
+    }
 
    /**
-	* Checks the employee salary as accepting
-	* only numerical values and return the validated input to the 
+    * Checks the employee salary as accepting
+    * only numerical values and return the validated input to the 
     * Employee details class.
     * @return salary
-	*/
-	public String checkSalary(String salary) {
+    */
+    public String checkSalary(String salary) {
 	    
-		if (!salary.matches("[0-9]{3,}")) {
-			System.out.println("Invalid Input!!!...");
-			return EmployeeDetails.getEmployeeSalary();
-		} 
-		return salary;
-	}
+        if (!salary.matches("[0-9]{3,}")) {
+	    System.out.println("Invalid Input!!!...");
+	    return EmployeeDetails.getEmployeeSalary();
+	} 
+	    return salary;
+    }
 
    /**
-	* Validating employee joining date by checking day, month, year of default
-	* calendar method. If any input other than predefined values are given, then
-	* the method is called again to get desired input.
-	* @return date
-	*/
-	public Date dateValidation(String dateOfBirth) {
+    * Validating employee joining date by checking day, month, year of default
+    * calendar method. If any input other than predefined values are given, then
+    * the method is called again to get desired input.
+    * @return date
+    */
+    public Date dateValidation(String dateOfBirth) {
         try {
-	        LocalDate inputDate = LocalDate.parse(dateOfBirth);
-	        LocalDate currentDate = LocalDate.now();
+	    LocalDate inputDate = LocalDate.parse(dateOfBirth);
+	    LocalDate currentDate = LocalDate.now();
 		   
-	        if (currentDate.isAfter(inputDate)) {
-	            return Date.valueOf(inputDate);
-	        }
-		} catch (Exception exception) {
-		   System.out.println("Invalid Input!!!..");
-		   return EmployeeDetails.getDateOfBirth();
-		}
-         return Date.valueOf(dateOfBirth);
-	}
+	    if (currentDate.isAfter(inputDate)) {
+	        return Date.valueOf(inputDate);
+	    }
+	} catch (Exception exception) {
+            System.out.println("Invalid Input!!!..");
+            return EmployeeDetails.getDateOfBirth();
+        }
+            return Date.valueOf(dateOfBirth);
+    }
  }
 	
 
